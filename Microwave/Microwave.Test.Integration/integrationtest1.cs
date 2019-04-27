663615a3337534ae379dd8ccf3fe216b68ac7980
MortenBech <mortenbech22@hotmail.com>
2019-04-27 14:35:41 +0200

    PowerTube now correctly displays between 1-100% instead of 50-700%

---
+++ Microwave/Microwave.Test.Integration/integrationtest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microwave.Test.Integration
+{
+    [TestClass]
+    public class Integrationtest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
