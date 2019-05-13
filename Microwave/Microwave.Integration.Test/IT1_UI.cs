4bd95b50f963b7eeef6fcee04d0a5224bee78107
MortenBech <mortenbech22@hotmail.com>
2019-05-13 16:10:47 +0200

    IT1 implemented. IT7 is being unused

--- Microwave/Microwave.Integration.Test/IT1_UI.cs
+++ Microwave/Microwave.Integration.Test/IT1_UI.cs
@@ -1,124 +1,96 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using MicrowaveOvenClasses.Boundary;
-//using MicrowaveOvenClasses.Interfaces;
-//using NSubstitute;
-//using NUnit.Framework;
-//using MicrowaveOvenClasses.Controllers;
-//using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Controllers;
+using System.Threading;
 
-//namespace Microwave.Integration.Test
-//{
-
-
-//    [TestFixture]
-//    public class IT1_UI
-//    {
-
-//        private IDoor door;
-//        private IUserInterface ui;
-//        private ITimer timer;
-//        private IPowerTube power;
-//        private IDisplay display;
-//        private ILight light;
-//        private IButton powerbtn;
-//        private IButton timerbtn;
-//        private IButton startCbtn;
-//        private ICookController cc;
-//        private IOutput output;
+namespace Microwave.Integration.Test
+{
 
 
+    [TestFixture]
+    public class IT1_UI
+    {
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            powerbtn = new Button();
-//            timerbtn = new Button();
-//            startCbtn = new Button();
-//            door = new Door();
-            
-//            ui = new UserInterface(powerbtn, timerbtn, startCbtn, door, display, light, cc);
+        private IDoor door;
+        private IUserInterface ui;
+        private ITimer timer;
+        private IPowerTube power;
+        private IDisplay display;
+        private ILight light;
+        private IButton powerbtn;
+        private IButton timerbtn;
+        private IButton startCbtn;
+        private ICookController cc;
+        private IOutput output;
 
 
 
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            startCbtn = new Button();
+            timerbtn = new Button();
+            powerbtn = new Button();
+            door = new Door();
 
+            output = Substitute.For<IOutput>();
 
-//        [Test] //Tester for om Output klassen modtager data fra this og de andre udnyttede klasser
-//        public void isCooking_StartingprintingtoOutput()
-//        {
-//            //StartCooking accesser funktionerne TurnOn og Timer Start
-//            cook.StartCooking(50, 5);
-//            //ToLower formater strengen til lowcase characters
-//            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("powertube works with 7,1 %")));
+            light = Substitute.For<ILight>();
+            timer = Substitute.For<ITimer>();
+            display = Substitute.For<IDisplay>();
+            power = Substitute.For<IPowerTube>();
 
-//        }
-//        [Test] //Tester for om Output klassen modtager data fra this og de andre udnyttede klasser
-//        /*public void isCooking_StartingprintingtoOutput()
-//        {
-//            //StartCooking accesser funktionerne TurnOn og Timer Start
-//            cook.StartCooking(50, 5);
-//            //ToLower formater strengen til lowcase characters
-//            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("powertube works with 7,1 %")));
+            cc = Substitute.For<ICookController>();
+            ui = new UserInterface(powerbtn, timerbtn, startCbtn, door, display, light, cc);
+        }
 
-//        }*/
+        [Test]
+        public void openDoor_LightTurnedOn()
+        {
+            door.Open();
+            light.Received().TurnOn();
+        }
 
-//        [TestCase] //Tester for om tiden vi modtager fra Timer klasses er det forventede vi får i this
-//        public void isTimeRemaining_theexpected()
-//        {
-//            cook.StartCooking(90, 1);
-//            ManualResetEvent pause = new ManualResetEvent(false);
+        [Test]
+        public void closeDoor_LightTurnedOff()
+        {
+            door.Open();
+            door.Close();
+            light.Received().TurnOff();
+        }
 
-//            pause.WaitOne(1500);
+        [Test]
+        public void powerButton_DisplayPower()
+        {
+            powerbtn.Press();
+            display.Received().ShowPower(50);
+        }
 
-//            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("powertube turned off")));
+        [Test]
+        public void timeButton_DisplayTime()
+        {
+            powerbtn.Press();
+            timerbtn.Press();
+            display.Received().ShowTime(1,0);
+        }
 
-//        }
-
-
-//        [Test] //Tester for om tiden er udeløbet, dvs efter 10 sek bør man den dø
-//        public void isCookingthen_StoppingTime()
-//        {
-//            cook.StartCooking(90,1);
-
-//            ManualResetEvent pause = new ManualResetEvent(false);
-
-//            pause.WaitOne(1500);
-            
-
-//            cook.Stop();
-
-//            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("powertube turned off")));
-
-//        }
-
-//        [Test] //Tester For om Cooking overhovedet gider at cooke igennem Timer og Powertube
-//        public void isCooking_WhileOn()
-//        {
-//            cook.StartCooking(90, 5);
-//            //Assert.That(() => cook.StartCooking(90, 5), Throws.TypeOf<ApplicationException>());
-
-//            Assert.Throws<System.ApplicationException>(() => cook.StartCooking(90, 5));
-
-//        }
-        
-//        [Test] //Tester for om Stop() funktionen fungerer uden delays.  
-//        public void StopCookingWhilecookingOn()
-//        {
-            
-//            cook.StartCooking(90, 1);
-//            cook.Stop();
-
-//            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("powertube turned off")));
-
-//        }
-
-
-//    }
-//}
+        [Test]
+        public void startButton_StartCooking()
+        {
+            powerbtn.Press();
+            timerbtn.Press();
+            startCbtn.Press();
+            cc.Received().StartCooking(50, 60);
+        }
+    }
+}
 
 
 
